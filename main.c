44dc01d8afa86ae54b6445babd016c110338cdf3
João Dini <copperbrjdmdini@gmail.com>
2021-10-27 15:49:54 -0300

    main agora recebe como parâmetro, t
    Era assim que era pra fazer será?

--- main.c
+++ main.c
@@ -2,8 +2,10 @@
 
 int main(int argc, char *argv[])
 {
+    int t;
+    sscanf(argv[1], "%d", &t);
     if(argc == 2){
-        printf("%s\n", argv[1]);
+        printf("t = %d\n", t);
     }
 
     return 0;
